efc759fec3eda22ee316987acce775603e20a2ae
Todd Mpeli <toddowenmpeli@rocketmail.com>
2023-08-22 22:02:42 +0100

    Fixed the new tab opening up when the home page button link is clicked.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar: FC = () => {
 				}}
 			>
 				<div className={styles.nav}>
-					<Link target="blank" href={`/`}>
+					<Link target="" href={`/`}>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							version="1.1"
